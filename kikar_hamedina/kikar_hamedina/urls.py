892cf22d04aace703093a5abe719febe06268bbb
Eliezer Steinbock <eliesteinbock@gmail.com>
2014-01-07 00:44:42 +0200

    still trying to work out what's going on. didn't really do anything

--- kikar_hamedina/kikar_hamedina/urls.py
+++ kikar_hamedina/kikar_hamedina/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from core.views import index
 from core.views import person
 from core.views import party
+from core.views import status
 
 admin.autodiscover()
 
@@ -11,5 +12,6 @@ urlpatterns = patterns('',
     url(r'^api/', include('core.urls')),
     url(r'^$', index, name='index'),
     url(r'^person/(?P<person_id>\d+)/$', person, name='person'),
-    url(r'^party/(?P<party_id>\d+)/$', party, name='party')
+    url(r'^party/(?P<party_id>\d+)/$', party, name='party'),
+    url(r'^status/(?P<status_id>\d+)/$', status, name='status')
 )
