7ef64d46440d54887d3c9ea578ee995e662342af
Eliezer Steinbock <eliesteinbock@gmail.com>
2014-01-27 20:38:16 +0200

    added tags to admin

--- kikar_hamedina/core/admin.py
+++ kikar_hamedina/core/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from models import Party, Person, Facebook_Feed, Facebook_Status
+from models import Party, Person, Facebook_Feed, Facebook_Status, Tag
 
 admin.site.register(Party)
 admin.site.register(Person)
 admin.site.register(Facebook_Feed)
 admin.site.register(Facebook_Status)
+admin.site.register(Tag)
