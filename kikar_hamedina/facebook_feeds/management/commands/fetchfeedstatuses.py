d143ecdc51131ab2dc46cf01030cfa2f912e74ce
yotammanor <yotammanor@gmail.com>
2014-04-23 03:01:43 +0300

    yotam: add facebook_status_attachment models, and accompanying changes to fetchfeedstatuses command.

--- kikar_hamedina/facebook_feeds/management/commands/fetchfeedstatuses.py
+++ kikar_hamedina/facebook_feeds/management/commands/fetchfeedstatuses.py
@@ -1,6 +1,6 @@
 import datetime
 from optparse import make_option
-
+from collections import defaultdict
 from django.conf import settings
 from django.core.management.base import BaseCommand, CommandError
 from django.utils import timezone
@@ -8,7 +8,11 @@ import facebook
 from ...models import \
     Facebook_Feed as Facebook_Feed_Model, \
     Facebook_Status as Facebook_Status_Model, \
-    User_Token as User_Token_Model
+    User_Token as User_Token_Model, \
+    Facebook_Status_Attachment as Facebook_Status_Attachment_Model, \
+    Facebook_Status_Attachment_Media as Facebook_Status_Attachment_Media_Model
+
+LENGTH_OF_EMPTY_ATTACHMENT_JSON = 21
 
 DEFAULT_STATUS_SELECT_LIMIT_FOR_INITIAL_RUN = 1000
 DEFAULT_STATUS_SELECT_LIMIT_FOR_REGULAR_RUN = 20
@@ -17,17 +21,27 @@ DEFAULT_STATUS_SELECT_LIMIT_FOR_REGULAR_RUN = 20
 class Command(BaseCommand):
     args = '<feed_id>'
     help = 'Fetches a feed'
-    new_option = make_option('--initial',
-                             action='store_true',
-                             dest='initial',
-                             default=False,
-                             help='flag initial runs fql query with limit of {0} instead of regular limit of {1}'
-                             .format(DEFAULT_STATUS_SELECT_LIMIT_FOR_INITIAL_RUN,
-                                     DEFAULT_STATUS_SELECT_LIMIT_FOR_REGULAR_RUN))
+    option_initial = make_option('-i',
+                                 '--initial',
+                                 action='store_true',
+                                 dest='initial',
+                                 default=False,
+                                 help='Flag initial runs fql query with limit of {0} instead of regular limit of {1}.'
+                                 .format(DEFAULT_STATUS_SELECT_LIMIT_FOR_INITIAL_RUN,
+                                         DEFAULT_STATUS_SELECT_LIMIT_FOR_REGULAR_RUN)
+    )
+    option_force_attachment_update = make_option('-a',
+                                                 '--force-attachment-update',
+                                                 action='store_true',
+                                                 dest='force-attachment-update',
+                                                 default=False,
+                                                 help='Use this flag to force updating of status attachment'
+    )
     option_list_helper = list()
     for x in BaseCommand.option_list:
         option_list_helper.append(x)
-    option_list_helper.append(new_option)
+    option_list_helper.append(option_initial)
+    option_list_helper.append(option_force_attachment_update)
     option_list = tuple(option_list_helper)
 
     graph = facebook.GraphAPI()
@@ -39,7 +53,7 @@ class Command(BaseCommand):
                 """
         select_self_published_status_query = """
                 SELECT
-                    post_id, message, created_time, like_info, comment_info, share_count, updated_time
+                    post_id, message, created_time, like_info, comment_info, share_count, updated_time, attachment, type
                 FROM
                     stream
                 WHERE
@@ -48,14 +62,115 @@ class Command(BaseCommand):
 
         return self.graph.fql(query=select_self_published_status_query)
 
-    def insert_status_object_to_db(self, status_object, feed_id):
+    @staticmethod
+    def insert_status_attachment(status, status_object):
+        attachment_defaultdict = defaultdict(str, status_object['attachment'])
+        attachment = Facebook_Status_Attachment_Model(
+            status=status,
+            name=attachment_defaultdict['name'],
+            caption=attachment_defaultdict['caption'],
+            description=attachment_defaultdict['description'],
+            link=attachment_defaultdict['href'],
+            facebook_object_id=attachment_defaultdict['fb_object_id']
+        )
+        attachment.save()
+        # add all media files related to attachment
+        for media_file in status_object['attachment']['media']:
+            media_file_defaultdict = defaultdict(str, media_file)
+
+            media = Facebook_Status_Attachment_Media_Model(
+                attachment=attachment,
+                type=media_file_defaultdict['type'],
+                link=media_file_defaultdict['href'],
+                alt=media_file_defaultdict['alt'],
+                thumbnail=media_file_defaultdict['src'],
+            )
+            if media.type == 'photo':
+                media.picture = media_file_defaultdict['picture']
+                media.owner_id = media_file_defaultdict['photo']['owner']
+            media.save()
+
+
+    @staticmethod
+    def get_unique_link_for_media(attachment, i, media_file):
+        media_file_defaultdict = defaultdict(str, media_file)
+        if media_file_defaultdict['href']:
+            unique_link = media_file_defaultdict['href']
+        elif 'music' in media_file_defaultdict.keys():
+            unique_link = media_file_defaultdict['music']['source_url']
+        else:
+            unique_link = '%s_media_%s' % (attachment.status.status_id, i)
+        return media_file_defaultdict, unique_link
+
+    @staticmethod
+    def update_status_attachment(attachment, status_object):
+
+        #Update relevant attachment fields
+        attachment_defaultdict = defaultdict(str, status_object['attachment'])
+        attachment.name = attachment_defaultdict['name']
+        attachment.caption = attachment_defaultdict['caption']
+        attachment.description = attachment_defaultdict['description']
+        attachment.link = attachment_defaultdict['href']
+        attachment.facebook_object_id = attachment_defaultdict['fb_object_id']
+        attachment.save()
+
+        # add or update all media files related to attachment
+        try:
+            for i, media_file in enumerate(attachment_defaultdict['media']):
+
+                media_file_defaultdict, unique_link = Command.get_unique_link_for_media(attachment, i, media_file)
+                media, created = Facebook_Status_Attachment_Media_Model.objects.get_or_create(attachment=attachment,
+                                                                                              link=unique_link)
+                media.type = media_file_defaultdict['type']
+                media.link = media_file_defaultdict['href']
+                media.alt = media_file_defaultdict['alt']
+                media.thumbnail = media_file_defaultdict['src']
+                if media.type == 'photo':
+                    media.picture = media_file_defaultdict['picture']
+                    media.owner_id = media_file_defaultdict['photo']['owner']
+                media.save()
+        except:
+            raise
+        #delete a media if no longer appears in retrieved attachment but appears in db
+        list_of_links = list()
+        for i, media_file in enumerate(status_object['attachment']['media']):
+            media_file_defaultdict, unique_link = Command.get_unique_link_for_media(attachment, i, media_file)
+            list_of_links.append(unique_link)
+
+        for media in attachment.media.all():
+            if media.link not in list_of_links:
+                print '%s!=%s' % (media.link, list_of_links)
+                media.delete()
+
+        # if all media are deleted, and attachment has no link - then there's no attachment, and it must be deleted
+        if not attachment.link and not attachment.media.all():
+            print 'deleting attachment'
+            attachment.delete()
+
+    def create_or_update_attachment(self, status, status_object):
+        """
+        If attachment exists, create or update all relevant fields.
+        """
+        print status.status_id
+        if len(str(status_object['attachment'])) > LENGTH_OF_EMPTY_ATTACHMENT_JSON:
+            attachment, created = Facebook_Status_Attachment_Model.objects.get_or_create(
+                status=status)
+            print 'I have an attachment. Created now: %s; Length of data: %d; id: %s' % (
+                created, len(str(status_object['attachment'])), status.status_id)
+            self.update_status_attachment(attachment, status_object)
+        else:
+            print 'i don''t have an attachment; Length of data: %d; id: %s' % (
+                len(str(status_object['attachment'])), status.status_id)
+
+    def insert_status_object_to_db(self, status_object, feed_id, options):
         """
         Receives a single status object as retrieved from facebook-sdk, an inserts the status
         to the db.
-        """
+                """
         #Create a datetime object from int received in status_object
         current_time_of_update = datetime.datetime.fromtimestamp(status_object['updated_time'],
                                                                  tz=timezone.utc)
+
         try:
             # If post_id already exists in DB
             status = Facebook_Status_Model.objects.get(status_id=status_object['post_id'])
@@ -66,6 +181,12 @@ class Command(BaseCommand):
                 status.comment_count = status_object['comment_info']['comment_count']
                 status.share_count = status_object['share_count']
                 status.updated = current_time_of_update
+                # update attachment data
+                self.create_or_update_attachment(status, status_object)
+            elif options['force-attachment-update']:
+                # force update of attachment, regardless of time
+                status.save()
+                self.create_or_update_attachment(status, status_object)
             else:
                 # If post_id exists but of equal or later time (unlikely, but may happen), disregard
                 self.stdout.write('status id {0} is already up-to-date.'.format(status_object['post_id']))
@@ -80,6 +201,10 @@ class Command(BaseCommand):
                                            published=datetime.datetime.fromtimestamp(
                                                int(status_object['created_time'])),
                                            updated=current_time_of_update)
+            if len(str(status_object['attachment'])) > LENGTH_OF_EMPTY_ATTACHMENT_JSON:
+                #There's an attachment
+                status.save()
+                self.insert_status_attachment(status, status_object)
         finally:
             # save status object.
             status.save()
@@ -125,10 +250,10 @@ class Command(BaseCommand):
         if len(args) == 0:
             for feed in Facebook_Feed_Model.objects.all():
                 self.stdout.write('Working on feed: {0}.'.format(feed.pk))
-                feed_statuses = self.get_feed_statuses(feed,fql_limit)
+                feed_statuses = self.get_feed_statuses(feed, fql_limit)
                 self.stdout.write('Successfully fetched feed: {0}.'.format(feed.pk))
                 for status in feed_statuses['statuses']:
-                    self.insert_status_object_to_db(status, feed_statuses['feed_id'])
+                    self.insert_status_object_to_db(status, feed_statuses['feed_id'], options)
                 self.stdout.write('Successfully written feed: {0}.'.format(feed.pk))
             self.stdout.write('Successfully fetched all')
 
@@ -142,15 +267,13 @@ class Command(BaseCommand):
             except Facebook_Feed_Model.DoesNotExist:
                 raise CommandError('Feed "%s" does not exist' % feed_id)
 
-            feed_statuses = self.get_feed_statuses(feed,fql_limit)
+            feed_statuses = self.get_feed_statuses(feed, fql_limit)
             for status in feed_statuses['statuses']:
-                self.insert_status_object_to_db(status, feed_statuses['feed_id'])
+                self.insert_status_object_to_db(status, feed_statuses['feed_id'], options)
             self.stdout.write('Successfully written feed: {0}.'.format(feed.id))
 
         # Case invalid args
         else:
             raise CommandError('Please enter a valid feed id')
-
-
 
         self.stdout.write('Successfully saved all statuses to db.')
--- kikar_hamedina/facebook_feeds/models.py
+++ kikar_hamedina/facebook_feeds/models.py
@@ -6,7 +6,6 @@ from django.contrib.contenttypes import generic
 # Create your models here.
 
 
-
 class Facebook_Feed_Generic(models.Model):
     content_type = models.ForeignKey(ContentType)
     object_id = models.PositiveIntegerField()
@@ -61,7 +60,7 @@ class Feed_Popularity(models.Model):
     def __unicode__(self):
         return unicode(self.feed) + " " + str(self.date_of_creation)
                # strftime("%Y_%M_%D_%H:%m:%s", self.date_of_creation)
-#
+
 
 class Facebook_Status(models.Model):
     feed = models.ForeignKey('Facebook_Feed')
@@ -84,6 +83,64 @@ class Facebook_Status(models.Model):
         split_status_id = self.status_id.split('_')
         return 'https://www.facebook.com/%d/posts/%d' % (split_status_id[0], split_status_id[1])
 
+status_with_photo = '161648040544835_720225251353775'
+status_with_youtube_link = '161648040544835_723225304387103'
+status_with_link_to_newspaper = '161648040544835_719797924729841'
+status_with_no_multimedia = '161648040544835_718801471496153'
+
+ATTACHMENT_MEDIA_TYPES = (
+    ('status', 'Status'),
+    ('video', 'Video'),
+    ('photo', 'Photo'),
+    ('link', 'Link'),
+)
+
+# """
+#
+# https://developers.facebook.com/docs/reference/fql/stream/
+#
+# The type of this story. Possible values are:
+# 11 - Group created
+# 12 - Event created
+# 46 - Status update    # Relevant
+# 56 - Post on wall from another user
+# 66 - Note created
+# 80 - Link posted     # Relevant
+# 128 -Video posted      # Relevant
+# 247 - Photos posted     # Relevant
+# 237 - App story
+# 257 - Comment created
+# 272 - App story
+# 285 - Checkin to a place
+# 308 - Post in Group
+# """
+
+
+class Facebook_Status_Attachment(models.Model):
+
+    status = models.OneToOneField(Facebook_Status, related_name='attachment')
+    name = models.TextField(null=True)  #name
+    caption = models.TextField(null=True)  #caption
+    description = models.TextField(null=True)  #description
+    link = models.URLField(null=True, max_length=512)  #href
+    facebook_object_id = models.CharField(unique=False, null=True, max_length=128)  #fb_object_id (exists only for internal links)
+
+    @property
+    def is_internal_link(self):
+        return 'https://www.facebook.com/' in self.link
+
+
+class Facebook_Status_Attachment_Media(models.Model):
+    attachment = models.ForeignKey(Facebook_Status_Attachment, related_name='media')
+    type = models.CharField(null=True, choices=ATTACHMENT_MEDIA_TYPES, max_length=16)  #type
+    link = models.URLField(null=True, max_length=512)  #href
+    alt = models.TextField(null=True) #alt
+    picture = models.URLField(null=True, max_length=512)  #picture
+    thumbnail = models.URLField(null=True, max_length=512)  # src
+    owner_id = models.TextField(null=True)  # photo.owner
+
+    class Meta:
+        unique_together = ('attachment', 'link')
 
 class User_Token(models.Model):
     token = models.CharField(max_length=256, unique=True)
