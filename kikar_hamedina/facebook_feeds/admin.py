59f9431f5025a756d2aa91c91a602a8902afb04e
yotammanor <yotammanor@gmail.com>
2014-05-04 03:14:04 +0300

    yotam: register attachment to admins

--- kikar_hamedina/facebook_feeds/admin.py
+++ kikar_hamedina/facebook_feeds/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 
-from models import Facebook_Feed, Facebook_Status, Tag, User_Token, Feed_Popularity
+from models import Facebook_Feed, \
+    Facebook_Status, \
+    Tag, \
+    User_Token, Feed_Popularity, Facebook_Feed_Generic, \
+    Facebook_Status_Attachment, Facebook_Status_Attachment_Media
 
 
 admin.site.register(Facebook_Feed)
@@ -8,3 +12,5 @@ admin.site.register(Facebook_Status)
 admin.site.register(Tag)
 admin.site.register(User_Token)
 admin.site.register(Feed_Popularity)
+admin.site.register(Facebook_Status_Attachment)
+admin.site.register(Facebook_Status_Attachment_Media)
