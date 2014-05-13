977990388431657fd7ff1cef7ddfd67961faccfb
Agam Rafaeli <agam.rafaeli@gmail.com>
2014-05-13 16:46:27 +0300

    First API commit - using TastyPie
    Added Tastypie to the installed apps
    Created the basic resources need in the api.py file
    Added the needed urls

--- kikar_hamedina/core/api.py
+++ kikar_hamedina/core/api.py
@@ -1,48 +1,57 @@
-# from django.http import Http404
-# from django.views.generic import View
-# from rest_framework import status
-# from rest_framework.decorators import api_view
-# from rest_framework.views import APIView, View
-# from rest_framework.response import Response
-# from rest_framework import generics
-# from rest_framework import mixins
-# from rest_framework import permissions
-# from rest_framework import filters
-# from rest_framework.reverse import reverse
-# from .serializers import PersonSerializer, PartySerializer, FacebookStatusSerializer, FacebookFeedSerializer
-# from .models import Facebook_Status, Facebook_Feed
-# from persons.models import Person, Party
-#
-# class PersonsList(generics.ListAPIView):
-#     queryset = Person.objects.all()
-#     serializer_class = PersonSerializer
-#
-#
-# class PersonDetail(generics.RetrieveAPIView):
-#     queryset = Person.objects.all()
-#     serializer_class = PersonSerializer
-#
-#
-# class StatusList(generics.ListAPIView):
-#     queryset = Facebook_Status.objects.all()
-#     serializer_class = FacebookStatusSerializer
-#     filter_backends = (filters.DjangoFilterBackend,)
-#     filter_fields = ('id', 'like_count', 'feed')
-#
-#
-# class FeedList(generics.ListAPIView):
-#     queryset = Facebook_Feed.objects.all()
-#     serializer_class = FacebookFeedSerializer
-#
-#
-# class PartyDetail(generics.RetrieveAPIView):
-#     queryset = Party.objects.select_related()
-#     serializer_class = PartySerializer
-#
-#
-# class PartiesList(generics.ListAPIView):
-#     queryset = Party.objects.all()
-#     serializer_class = PartySerializer
-#     permission_classes = [
-#         permissions.AllowAny
-#     ]
+from tastypie import fields
+from tastypie.resources import ModelResource
+from mks.models import Knesset, Party, Member
+from facebook_feeds.models import Facebook_Status, Facebook_Feed, Tag, User_Token, Feed_Popularity
+from planet.models import Blog
+
+class MemberResource(ModelResource): pass
+class Facebook_StatusResource(ModelResource): pass
+
+
+class KnessetResource(ModelResource):
+	class Meta:
+		queryset = Knesset.objects.all()
+		resource_name = 'knesset'
+
+class PartyResource(ModelResource):
+	knesset = fields.ForeignKey(KnessetResource,'knesset',null=True, blank=True)
+	all_members = fields.ManyToManyField(MemberResource, 'all_members')
+	class Meta:
+		queryset = Party.objects.all()
+		resource_name = 'party'
+
+class MemberResource(ModelResource):
+	current_party = fields.ForeignKey(PartyResource, 'current_party', null=True, blank=True)
+	parties = fields.ManyToManyField(PartyResource, 'parties')
+	blog = fields.OneToOneField(Blog, 'blog', blank=True, null=True)
+	class Meta:
+		queryset = Member.objects.all()
+		resource_name = 'member'
+
+class Facebook_FeedResource(ModelResource):
+	class Meta:
+		queryset = Facebook_Feed.objects.all()
+		resource_name = 'facebook_feed'
+
+class TagResource(ModelResource):
+	statuses = fields.ManyToManyField(Facebook_StatusResource,'statuses')
+
+	class Meta:
+		queryset = Tag.objects.all()
+		resource_name = 'tag'
+
+class Facebook_StatusResource(ModelResource):
+	feed = fields.ForeignKey(Facebook_FeedResource, 'feed')
+	tags = fields.ManyToManyField(TagResource, 'tags')
+	class Meta:
+		queryset = Facebook_Status.objects.all()
+		resource_name = 'facebook_status'
+
+	def dehydrate(self, bundle):
+		bundle.data['facebook_link'] = bundle.obj.get_link
+
+		if bundle.obj.has_attachment:
+			bundle.data['attachment'] = "Attachment"
+		return bundle
+
+
