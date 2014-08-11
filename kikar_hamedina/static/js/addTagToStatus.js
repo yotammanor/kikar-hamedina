b60c75723f47565a20b0978c6f90708fc1dea068
jordan <jordan@jordan-HP-ProBook-4540s>
2014-08-11 22:11:21 +0300

    fixed comma seperated tags

--- kikar_hamedina/static/js/addTagToStatus.js
+++ kikar_hamedina/static/js/addTagToStatus.js
@@ -3,30 +3,36 @@ $(document).ready(function () {
 		id = $(this).parent().data("statusid")
 		tag = $(this).parent().find(".add-tag-input").val()
 		$(this).parent().find(".add-tag-input").val("")
+		var tags = (String(tag)).split(",")
+		console.log(tags)
+		
+		for (t in tags){
+			url = "/add_tag_to_status/?id="+id+"&tag_str="+tags[t]
 
-		url = "/add_tag_to_status/?id="+id+"&tag_str="+tag
-
-		$.ajax({
-			url: url,
-			contentType: "application/json",
-			success: function (data) {
-				console.log("GOT RESPONSE")
-				if (data['success']) {
-					tag = data['tag']
-					var source 		= $("#add-tag-to-status-template").html()
-					var template 	= Handlebars.compile(source);
-					var html    	= template(tag);
-					$("#" + data['id'] + "-tags").prev().css("display","inline")
-					$("#" + data['id'] + "-tags").append(html)
+			$.ajax({
+				url: url,
+				contentType: "application/json",
+				success: function (data) {
+					console.log("GOT RESPONSE")
+					if (data['success']) {
+						tag = data['tag']
+						console.log(tag)
+						var source 		= $("#add-tag-to-status-template").html()
+						var template 	= Handlebars.compile(source);
+						var html    	= template(tag);
+						console.log("html: " + html)
+						$("#" + data['id'] + "-tags").prev().css("display","inline")
+						$("#" + data['id'] + "-tags").append(html)
+					}
+					else {
+						console.log(data)
+						console.log("ERROR AT SERVER")
+					}
+				},
+				error: function(data) {
+					console.log("BADDDDD ERROR!!")
 				}
-				else {
-					console.log(data)
-					console.log("ERROR AT SERVER")
-				}
-			},
-			error: function(data) {
-				console.log("BADDDDD ERROR!!")
-			}
-		});
+			});
+		}
 	});
 })
