cf3ef9702668af3c0b6e36adcb0b936f806e6c0e
jordan <jordan@jordan-HP-ProBook-4540s>
2014-08-26 10:43:13 +0300

    autocomplete for tags (still not fully working)

--- kikar_hamedina/static/js/addTagToStatus.js
+++ kikar_hamedina/static/js/addTagToStatus.js
@@ -1,9 +1,12 @@
 $(document).ready(function () {
+
 	$(".add-tag-button").click(function () {
+		console.log("adding tag")
 		id = $(this).parent().data("statusid")
 		tag = $(this).parent().find(".add-tag-input").val()
 		$(this).parent().find(".add-tag-input").val("")
 		var tags = (String(tag)).split(",")
+		console.log(tag)
 		console.log(tags)
 		
 		for (t in tags){
@@ -14,6 +17,7 @@ $(document).ready(function () {
 				contentType: "application/json",
 				success: function (data) {
 					console.log("GOT RESPONSE")
+					console.log(data)
 					if (data['success']) {
 						tag = data['tag']
 						console.log(tag)
@@ -35,4 +39,41 @@ $(document).ready(function () {
 			});
 		}
 	});
+    $(".add-tag-input").keydown(function (event) {
+//        $('#add-tag-dropdown').removeClass("open")
+        inputText = $(".add-tag-input").val()
+        id = $(this).parent().parent().data("statusid")
+        url = "/search_bar/?text=" + inputText
+		
+        if (inputText.length > 1) {
+            $.ajax({
+                url: url,
+                contentType: "application/json",
+                success: function (data) {
+
+                    $('#add-tag-list').html('')
+//                	$('.dropdown-menu').html('')
+                    for (var i = 0; i < data['number_of_results']; i++) {
+                        var result = data['results'][i]
+                		console.log(result)
+                        if (result['type'] == "tag") {
+                            var source = $("#add-tag-result-tag-list-item-template").html()
+                        }
+                        var template = Handlebars.compile(source);
+                        var html = template(result);
+                        $('#add-tag-list').append(html)
+                    }
+                    if (data['number_of_results'] > 0) {
+                        $('#add-tag-dropdown').addClass("open")
+                    }
+                },
+				error: function(data) {
+					console.log("BADDDDD ERROR!!")
+					console.log(data)
+				}
+
+            });
+        }
+        
+    });
 })
