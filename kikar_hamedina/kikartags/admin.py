fa4218e4e8cb358117098fa06b1e9b936df283e1
yotammanor <yotammanor@gmail.com>
2014-11-16 01:09:43 +0200

    yotam: Improved usabilty of kikartags admin pages

--- kikar_hamedina/kikartags/admin.py
+++ kikar_hamedina/kikartags/admin.py
@@ -1,9 +1,22 @@
 from django.contrib import admin
 
-from kikartags.models import Tag, TaggedItem, TagSynonym
+from kikartags.models import Tag
+from kikartags.models import TaggedItem, TagSynonym
 
 
-admin.site.register(Tag)
-admin.site.register(TaggedItem)
-admin.site.register(TagSynonym)
+class TaggedItemInline(admin.TabularInline):
+    model = TaggedItem
+    extra = 1
+    max_num = 5
 
+
+class TagSynonymInline(admin.TabularInline):
+    model = TagSynonym
+    extra = 1
+    fk_name = 'proper_form_of_tag'
+
+
+class TagAdmin(admin.ModelAdmin):
+    inlines = (TagSynonymInline, TaggedItemInline)
+
+admin.site.register(Tag, TagAdmin)
