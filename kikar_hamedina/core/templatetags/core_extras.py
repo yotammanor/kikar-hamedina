3ee344191357d8942260614bc01fc4e1d18be547
shygon <shai.b.l@gmail.com>
2014-12-21 09:56:53 +0000

    Use the "link" filter instead of "embed_link" for modal posts - works for all page types, including Kalderon and Yishai

--- kikar_hamedina/core/templatetags/core_extras.py
+++ kikar_hamedina/core/templatetags/core_extras.py
@@ -9,13 +9,8 @@ register = template.Library()
 @register.filter(name='link')
 def link(status):
     """Returns the post link for a status"""
-    return 'https://www.facebook.com/%s/posts/%s' % (status.status_id.split('_')[0], status.status_id.split('_')[1])
-
-
-@register.filter(name='embed_link')
-def link(status):
-    """Returns the embed link for a post"""
-    return '%s/posts/%s' % (status.feed.link, status.status_id.split('_')[1])
+    split_id = status.status_id.split('_')
+    return 'https://www.facebook.com/%s/posts/%s' % (split_id[0], split_id[1])
 
 
 @register.filter(name='percent')
