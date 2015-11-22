c76146fb35297c6eba2b516e26d28e334174b8cc
Udi Oron <udioron@gmail.com>
2015-11-22 14:29:13 +0200

    Fixed zinnia required changes. Run `./manage.py migrate --fake zinnia 0001` and `./manage.py migrate zinnia` if strange DB errors occur.

--- kikar_hamedina/core/urls.py
+++ kikar_hamedina/core/urls.py
@@ -27,7 +27,7 @@ urlpatterns = patterns('',
                        url(r'^$', views.AllStatusesView.as_view(), name='index'),
                        # include urls
                        url(r'^api/', include(v1_api.urls)),
-                       url(r'^blog/', include('zinnia.urls')),
+                       url(r'^blog/', include('zinnia.urls', namespace='zinnia')),
                        url(r'^comments/', include('django_comments.urls')),
                        # navbar direct decendants url
                        url(r'^billboards/$', views.BillboardsView.as_view(), name='billboards'),
