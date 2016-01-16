7d2af1f44b83a2299801c48f821a556337aa7b7a
guytepper <guytepper@gmail.com>
2016-01-16 20:33:55 +0200

    added focusController to handle up/down key events in search results

--- kikar_hamedina/static/js/searchBar.js
+++ kikar_hamedina/static/js/searchBar.js
@@ -15,10 +15,13 @@
 }
 
 $(document).ready(function () {
-//    $("#navbar-search-box").input(function (event) {
+
+    var resultList, // search results jQuery object
+        tabindex;
+
     $("#navbar-search-box").bind({
+        // event: When a letter is typed in, the auto-suggest updates
         input: function (event) {
-//          event - when a letter is typed in, the auto-suggest updates
             $('#search-results-dropdown').removeClass("open")
             inputText = $("#navbar-search-box").val()
             url = "/search_bar/?text=" + inputText
@@ -27,6 +30,7 @@ $(document).ready(function () {
                     url: url,
                     contentType: "application/json",
                     success: function (data) {
+                        tabindex = 0; // only first result is focusable
                         $('#search-results-list').html('')
                         for (var i = 0; i < data['number_of_results']; i++) {
                             var result = data['results'][i]
@@ -39,19 +43,27 @@ $(document).ready(function () {
                             else if (result['type'] == "tag") {
                                 var source = $("#result-tag-list-item-template").html()
                             }
+
+                            if ( i > 0 && tabindex == 0 ) {
+                              tabindex = -1;
+                            }
+                            result['tabindex'] = tabindex;
+
                             var template = Handlebars.compile(source);
                             var html = template(result);
                             $('#search-results-list').append(html)
                         }
                         if (data['number_of_results'] > 0) {
                             $('#search-results-dropdown').addClass("open")
+                            resultList = $('.result-container a');
                         }
                     }
                 });
             }
+
         },
 
-//      event: Enter-key is pressed while typing - equivalent to submit-search
+        // event: Enter-key is pressed while typing - equivalent to submit-search
         keydown: function (event) {
             if (event.which == 13) {
                 var uri = buildURIWithQuotes($("#search-bar-submit").data('target'));
@@ -61,16 +73,53 @@ $(document).ready(function () {
         }
     });
 
-// event: click on submit search button
+    // event: Up / Down keys navigation in search results
+    $("#search-results-list").bind('keydown', focusController);
+
+    // event: Click on submit search button
     $("#search-bar-submit").click(function () {
         var uri = buildURIWithQuotes($("#search-bar-submit").data('target'));
         window.location.assign(window.location.origin + encodeURI(uri))
     });
+
+
+    var key, currentIndex, previousElm, nextElm;
+    function focusController(e) {
+    // Resets the tabindex for the current focused element and sets
+    // the tabindex for the next/previous element + focuses it
+
+        // finds current focused element
+        resultList.each(function(i, elm) {
+          if ( elm.getAttribute('tabindex') == 0 ) {
+            currentIndex = i;
+            return false;
+          }
+        });
+
+        thisElm = resultList[currentIndex];
+        nextElm = resultList[currentIndex + 1];
+        prevElm = resultList[currentIndex - 1];
+
+        key = e.which;
+
+        switch ( key ) {
+          case 40:
+            e.preventDefault();
+            if ( nextElm ) {
+              thisElm.setAttribute('tabindex', -1);
+              nextElm.setAttribute('tabindex', 0);
+              nextElm.focus();
+            }
+            break;
+
+          case 38:
+            e.preventDefault();
+            if ( prevElm ) {
+              thisElm.setAttribute('tabindex', -1);
+              prevElm.setAttribute('tabindex', 0);
+              prevElm.focus();
+            }
+            break;
+        }
+    }
 });
-
-
-
-
-
-
-
