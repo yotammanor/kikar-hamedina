8b5785cc4470d9ea2ef0a8c9eeb673c47eec087b
Yotam <yotammanor@gmail.com>
2016-01-18 21:25:09 +0200

    add link to different languages in navbar.

--- kikar_hamedina/kikar_hamedina/urls.py
+++ kikar_hamedina/kikar_hamedina/urls.py
@@ -1,6 +1,8 @@
 from solid_i18n.urls import solid_i18n_patterns as patterns
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.sitemaps.views import sitemap
+
 # from core.views import index
 # from core.views import person
 # from core.views import party
@@ -8,10 +10,14 @@ from django.contrib import admin
 
 admin.autodiscover()
 
+sitemaps = {}
+
 urlpatterns = patterns('',
                        url(r'^admin/', include(admin.site.urls)),
                        # url(r'^api/', include('core.urls')),
                        url(r'^', include('core.urls')),
+                       url(r'^sitemap\.xml$', sitemap, {'sitemaps': sitemaps},
+                           name='django.contrib.sitemaps.views.sitemap')
                        # url(r'^$', index, name='index'),
                        # url(r'^person/(?P<person_id>\d+)/$', person, name='person'),
                        # url(r'^party/(?P<party_id>\d+)/$', party, name='party'),
