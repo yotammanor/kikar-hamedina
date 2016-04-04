0c15bc353e479f3901c9b3d560de6f634dd5e541
Yotam <yotammanor@gmail.com>
2016-04-04 19:47:08 +0300

    review and changes to yuval's code.

--- kikar_hamedina/core/sitemap.py
+++ kikar_hamedina/core/sitemap.py
@@ -1,66 +1,57 @@
 from django.contrib.sitemaps import Sitemap
 from mks.models import Member, Party
+from core.models import MEMBER_MODEL, PARTY_MODEL
 from django.conf import settings
-from datetime import datetime
+from django.utils import timezone
+
+DEFAULT_MIN_DATE = timezone.make_aware(timezone.datetime(1970, 1, 1, 0, 0, 0, 0))
+
 import pytz
 
+
 class MkSitemap(Sitemap):
-    changefreq = "weekly"
+    changefreq = "daily"
     priority = 0.5
 
     def items(self):
-        return Member.objects.filter(is_current=True);
-
+        return MEMBER_MODEL.objects.filter(is_current=True)
 
     def lastmod(self, obj):
-        if (obj.facebook_persona != None and
-            # I have no idea why but the set is sometime empty
-            obj.facebook_persona.get_main_feed.facebook_status_set.order_by('-published').first() != None):
-
-            # TODO: I think I am not getting the correct date here, for example 867 supposed to have a different date (on my data of course)
-            #if (obj.id == 867):
-             #   print 'bla'
-              #  print 'for id 867 date is: ' + str(obj.facebook_persona.get_main_feed.facebook_status_set.order_by('-published').first().locally_updated);
-
-            return obj.facebook_persona.get_main_feed.facebook_status_set.order_by('-published').first().locally_updated;
-        else:
-            return datetime(1970,1,1,10,0,0,0, pytz.UTC);
+        if obj.facebook_persona and obj.facebook_persona.get_main_feed.locally_updated:
+            return obj.facebook_persona.get_main_feed.locally_updated
+        return DEFAULT_MIN_DATE
 
 
 class PartySitemap(Sitemap):
-    changefreq = "weekly"
+    changefreq = "daily"
     priority = 0.5
 
     def items(self):
-        return Party.objects.filter(knesset__number=settings.CURRENT_KNESSET_NUMBER);
+        return PARTY_MODEL.current_knesset.all()
 
-    def lastmod(self,obj):
-        maxDate = datetime(1970,1,1,10,0,0,0, pytz.UTC);
-        for m in Member.objects.filter(current_party=obj.id).all():
-            if (m.facebook_persona != None and
-             # I have no idea why but the set is sometime empty
-             m.facebook_persona.get_main_feed.facebook_status_set.order_by('-published').first() != None):
-                curLastUpdate = m.facebook_persona.get_main_feed.facebook_status_set.order_by('-published').first().locally_updated;
-                if maxDate < curLastUpdate:
-                    maxDate = curLastUpdate;
+    def lastmod(self, obj):
+        members = obj.current_members()
+        update_dates = [member.facebook_persona.get_main_feed.locally_updated for member in members if
+                        member.facebook_persona]
+        return max(update_dates + [DEFAULT_MIN_DATE])
 
-        return maxDate;
 
 class AbstractSitemapClass():
-
     url = None
+
     def get_absolute_url(self):
         return self.url
+
 
 class StaticViewSitemap(Sitemap):
     priority = 1
     changefreq = "daily"
 
     pages = {
-             'home':'/',
-             'billboards':'/billboards',
-             'about' : '/about',
-             }
+        'home': '/',
+        'billboards': '/billboards',
+        'about': '/about',
+    }
     main_sitemaps = []
     for page in pages.keys():
         sitemap_class = AbstractSitemapClass()
