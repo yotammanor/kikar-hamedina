78a2451d27bfee374ea8297d4d56a32fc4d23c81
Yotam <yotammanor@gmail.com>
2016-04-18 21:42:13 +0300

    add all-members page as "sponsored link" in search bar.

--- kikar_hamedina/static/js/searchBar.js
+++ kikar_hamedina/static/js/searchBar.js
@@ -1,54 +1,75 @@
 /* SearchBar.js - defines the behaviour of the search option that appears
-* in the navigation bar. It allows auto-suggest as you type (mk, party, tags)
-* and allows for free search.
-* */
+ * in the navigation bar. It allows auto-suggest as you type (mk, party, tags)
+ * and allows for free search.
+ * */
 
- function buildURIWithQuotes(SearchURI) {
+function buildURIWithQuotes(SearchURI) {
 //  This function receives target-uri for search,
 //  extracts input text from search box, add quotes to search string,
 //  and returns a ready-to-execute uri path with searc_str parameters.
 
-    var inputText = $("#navbar-search-box").val()
-    var url = SearchURI
-    var fixed_url = url + '?search_str="' + inputText + '"'
-    return fixed_url
+    var inputText = $("#navbar-search-box").val();
+    return SearchURI + '?search_str="' + inputText + '"';
+}
+
+function addDefaultBookMarks(count) {
+    var result = {};
+    result['count'] = count;
+    var source = $("#result-default-link-all-mks-template").html();
+    var template = Handlebars.compile(source);
+    var html = template(result);
+    $('#search-results-list').append(html);
 }
 
 $(document).ready(function () {
 
+    // append default link to all-members on load
+    var count = 0;
+    addDefaultBookMarks(count);
+
+
     var resultList, // search results jQuery object
         selectedIndex = -1; // selected search result: -1 means nothing is selected.
+
+    resultList = $('.result-container a');
 
     $("#navbar-search-box").bind({
         // event: When a letter is typed in, the auto-suggest updates
         input: function (event) {
             $('#search-results-dropdown').removeClass("open");
             this.removeAttribute('activedescendant');
-            inputText = $("#navbar-search-box").val()
-            url = "/search_bar/?text=" + inputText
+            var inputText = $("#navbar-search-box").val();
+            var url = "/search_bar/?text=" + inputText;
             if (inputText.length > 1) {
                 $.ajax({
                     url: url,
                     contentType: "application/json",
                     success: function (data) {
-                        $('#search-results-list').html('')
+                        var resultListDiv = $('#search-results-list');
+                        resultListDiv.html('');
+                        var source;
                         for (var i = 0; i < data['number_of_results']; i++) {
-                            var result = data['results'][i]
+                            var result = data['results'][i];
                             if (result['type'] == "party") {
-                                var source = $("#result-party-list-item-template").html()
+                                source = $("#result-party-list-item-template").html()
                             }
                             else if (result['type'] == "member") {
-                                var source = $("#result-member-list-item-template").html()
+                                source = $("#result-member-list-item-template").html()
                             }
                             else if (result['type'] == "tag") {
-                                var source = $("#result-tag-list-item-template").html()
+                                source = $("#result-tag-list-item-template").html()
                             }
 
                             result.count = i; // For Handlebars - id value on result output
                             var template = Handlebars.compile(source);
                             var html = template(result);
-                            $('#search-results-list').append(html)
+                            resultListDiv.append(html);
+
                         }
+                        // append default link to all-members
+                        count = data['number_of_results'].length;
+                        addDefaultBookMarks(count);
+
                         if (data['number_of_results'] > 0) {
                             $('#search-results-dropdown').addClass("open");
                             resultList = $('.result-container a');
@@ -56,6 +77,13 @@ $(document).ready(function () {
                         }
                     }
                 });
+            } else {
+                var resultListDiv = $('#search-results-list');
+                resultListDiv.html('');
+                count = 0;
+                addDefaultBookMarks(count);
+                resultList = $('.result-container a');
+                selectedIndex = -1;
             }
 
         },
@@ -65,27 +93,27 @@ $(document).ready(function () {
         //        Up / Down keys - navigation between search results
 
         keydown: function (event) {
-          var uri, full_url;
-          switch ( event.which ) {
-            case 13:
-                if ( selectedIndex > -1 ) {
-                  full_url = $('.active-result').attr('href');
-                }
-                else {
-                  uri = buildURIWithQuotes($("#search-bar-submit").data('target'));
-                  full_url = window.location.origin + encodeURI(uri);
-                }
-                window.location.assign(full_url)
-                break;
+            var uri, full_url;
+            switch (event.which) {
+                case 13:
+                    if (selectedIndex > -1) {
+                        full_url = $('.active-result').attr('href');
+                    }
+                    else {
+                        uri = buildURIWithQuotes($("#search-bar-submit").data('target'));
+                        full_url = window.location.origin + encodeURI(uri);
+                    }
+                    window.location.assign(full_url);
+                    break;
 
-            case 40:
-                setSelectedIndex(selectedIndex + 1);
-                break;
+                case 40:
+                    setSelectedIndex(selectedIndex + 1);
+                    break;
 
-            case 38:
-                setSelectedIndex(selectedIndex - 1);
-                break;
-          }
+                case 38:
+                    setSelectedIndex(selectedIndex - 1);
+                    break;
+            }
         }
 
     });
@@ -100,23 +128,23 @@ $(document).ready(function () {
     // sets the focus on the next/previous search result
     function setSelectedIndex(i) {
 
-      // if a result has been already selected
-      if ( selectedIndex > -1 ) {
-        resultList[selectedIndex].className = '';
-      }
+        // if a result has been already selected
+        if (selectedIndex > -1) {
+            resultList[selectedIndex].className = '';
+        }
 
-      // end of result list - set to first result
-      if ( i == resultList.length) {
-        i = 0;
-      }
+        // end of result list - set to first result
+        if (i == resultList.length) {
+            i = 0;
+        }
 
-      // first result - set to last result
-      if ( i < 0 ) {
-        i = resultList.length - 1;
-      }
+        // first result - set to last result
+        if (i < 0) {
+            i = resultList.length - 1;
+        }
 
-      resultList[i].className = 'active-result';
-      $("#navbar-search-box").attr('activedescendant', 'option-' + i);
-      selectedIndex = i;
+        resultList[i].className = 'active-result';
+        $("#navbar-search-box").attr('activedescendant', 'option-' + i);
+        selectedIndex = i;
     }
 });
