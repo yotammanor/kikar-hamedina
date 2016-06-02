ec215d19e7ebcc92127ec48b9476be4bf1b2a9a3
Yotam <yotammanor@gmail.com>
2016-06-03 00:04:15 +0300

    refactored fetchstatuslikes as KikarStatusCommand Subclass

--- kikar_hamedina/facebook_feeds/management/commands/fetchstatuslikes.py
+++ kikar_hamedina/facebook_feeds/management/commands/fetchstatuslikes.py
@@ -11,6 +11,7 @@ from django.conf import settings
 from django.core.management.base import BaseCommand, CommandError
 import facebook
 from facebook_feeds.models import Facebook_Status, User_Token, Facebook_User, Facebook_Like
+from facebook_feeds.management.commands.kikar_base_commands import KikarStatusCommand
 
 FACEBOOK_API_VERSION = getattr(settings, 'FACEBOOK_API_VERSION', 'v2.5')
 NUMBER_OF_TRIES_FOR_REQUEST = getattr(settings, 'NUMBER_OF_TRIES_FOR_REQUEST', 2)
@@ -22,58 +23,8 @@ SLEEP_TIME = 3
 DELETED_STATUS_ERROR_CODE = 100
 
 
-class Command(BaseCommand):
-    args = '<status_id>'
+class Command(KikarStatusCommand):
     help = 'Gets all Likes for a given status.'
-    option_list = BaseCommand.option_list + (
-        make_option('-f',
-                    '--force-update',
-                    action='store_true',
-                    dest='force-update',
-                    default=False,
-                    help='Force update of status.'),
-        make_option('-a',
-                    '--force-attachment-update',
-                    action='store_true',
-                    dest='force-attachment-update',
-                    default=False,
-                    help='Use this flag to force updating of status attachment'),
-        make_option('--from-date',
-                    action='store',
-                    type='string',
-                    dest='from-date',
-                    default=None,
-                    help='Specify date from which to update the statuses (inclusive) e.g. 2014-03-31'),
-        make_option('--to-date',
-                    action='store',
-                    type='string',
-                    dest='to-date',
-                    default=None,
-                    help='Specify date until which to update the statuses (exclusive) e.g. 2014-03-31'),
-        make_option('--feed-ids',
-                    action='store',
-                    type='string',
-                    dest='feed-ids',
-                    default=None,
-                    help='Specify particular feed ids you want to update likes for with list of ids (e.g. 51, 54)'),
-        make_option('--update-deleted',
-                    action='store_true',
-                    dest='update-deleted',
-                    default=False,
-                    help="Update is_deleted flag: set to True/False for deleted/existing statuses"),
-        make_option('--file',
-                    action='store',
-                    dest='file_path',
-                    default=None,
-                    help="execute on list of status ids from file"),
-        make_option('--skip',
-                    action='store_true',
-                    dest='skip',
-                    default=False,
-                    help="skip statuses that has data for them"),
-    )
-
-    graph = facebook.GraphAPI()
 
     def fetch_status_object_data(self, status_id, limit):
         """
@@ -190,8 +141,8 @@ class Command(BaseCommand):
             except AttributeError:
                 # exception - trying to set an empty token (NoneType) as graph.access_token
                 # Fallback: Set facebook graph access token to app access token
-                self.graph.access_token = facebook.get_app_access_token(settings.FACEBOOK_APP_ID,
-                                                                        settings.FACEBOOK_SECRET_KEY)
+                self.graph.access_token = self.graph.get_app_access_token(settings.FACEBOOK_APP_ID,
+                                                                          settings.FACEBOOK_SECRET_KEY)
                 if status.feed.requires_user_token:
                     # If the Status's Feed is set to require a user-token, and none exist in our db, the feed is skipped.
                     print('feed %d requires user token, skipping.' % status.id)
@@ -229,43 +180,7 @@ class Command(BaseCommand):
         post_number_limit = DEFAULT_STATUS_SELECT_LIMIT_FOR_REGULAR_RUN
         print('Variable post_number_limit set to: {0}.'.format(post_number_limit))
 
-        list_of_statuses = list()
-        # Case no args - fetch all statuses or by options
-        if len(args) == 0:
-            criteria = {}
-            if options['from-date']:
-                criteria['published__gte'] = dateutil.parser.parse(options['from-date'])
-            if options['to-date']:
-                criteria['published__lt'] = dateutil.parser.parse(options['to-date'])
-            if options['feed-ids']:
-                criteria['feed__id__in'] = options['feed-ids'].split(',')
-            db_statuses = Facebook_Status.objects_no_filters.filter(**criteria).order_by('-published')
-            list_of_statuses = list(db_statuses)
-
-            if options['file_path']:
-                with open(options['file_path'], 'r') as f:
-                    reader = DictReader(f)
-                    list_of_status_ids = [x['status_id'] for x in reader]
-                    list_of_statuses = Facebook_Status.objects.filter(status_id__in=list_of_status_ids)
-
-        # Case arg exists - fetch status by id supplied
-        elif len(args) == 1:
-            status_id = args[0]
-            try:
-                status = Facebook_Status.objects_no_filters.get(status_id=status_id)
-                list_of_statuses.append(status)
-
-            except Facebook_Status.DoesNotExist:
-                warning_msg = "Status #({0}) does not exist.".format(status_id)
-                logger = logging.getLogger('django')
-                logger.warning(warning_msg)
-                raise CommandError('Status "%s" does not exist' % status_id)
-
-        # Case invalid args
-        else:
-            raise CommandError('Please enter a valid status id')
-
-        self.stdout.write('Starting to update likes for {0} statuses'.format(len(list_of_statuses)))
+        list_of_statuses = self.parse_statuses(args, options)
 
         # Iterate over list_of_statuses
         for i, status in enumerate(list_of_statuses):
