7db66dc25e71537f2b610f9308b57099d550190a
Yotam <yotammanor@gmail.com>
2016-06-16 11:15:44 +0300

    fix worker execution 2

--- kikar_hamedina/facebook_feeds/management/commands/processcommentcontent.py
+++ kikar_hamedina/facebook_feeds/management/commands/processcommentcontent.py
@@ -8,8 +8,8 @@ from concurrent import futures
 class Command(KikarStatusCommand):
     help = 'Retrieve all comments for a status, process content text and save to db..'
 
-    def worker(self, i, comment, status, processor):
-        self.stdout.write('working on comment {} of {}'.format(i + 1, len(list_of_comments)))
+    def worker(self, i, comment, status, processor, num_of_comments):
+        self.stdout.write('working on comment {} of {}'.format(i + 1, num_of_comments))
         text = processor.text_manipulation_mk_names(text=comment.content, context_status=status)
         text = processor.text_manipulation_emojis(text=text)
         comment.processed_content = text
@@ -28,7 +28,7 @@ class Command(KikarStatusCommand):
         list_of_comments = Facebook_Status_Comment.objects.filter(parent__status_id__in=list_of_statuses)
         with futures.ThreadPoolExecutor(max_workers=options['workers']) as executer:
             for i, comment in enumerate(list_of_comments):
-                executer.submit(self.worker, i, comment, comment.parent, processor)
+                executer.submit(self.worker, i, comment, comment.parent, processor, len(list_of_comments))
         info_msg = "Successfully saved all statuses to db"
         logger = logging.getLogger('django')
         logger.info(info_msg)
