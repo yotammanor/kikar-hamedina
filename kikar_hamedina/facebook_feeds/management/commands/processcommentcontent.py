da48b29831867e3a3592fca2df1d60cb19e6e4d1
Yotam <yotammanor@gmail.com>
2016-06-16 11:43:00 +0300

    fix worker execution 4 - back to working on status

--- kikar_hamedina/facebook_feeds/management/commands/processcommentcontent.py
+++ kikar_hamedina/facebook_feeds/management/commands/processcommentcontent.py
@@ -8,8 +8,8 @@ from concurrent import futures
 class Command(KikarStatusCommand):
     help = 'Retrieve all comments for a status, process content text and save to db..'
 
-    def worker(self, i, comment, status, processor, num_of_comments):
-        self.stdout.write('working on comment {} of {}'.format(i + 1, num_of_comments))
+    def worker(self, i, comment, status, processor):
+        self.stdout.write('working on comment {}'.format(i))
         text = processor.text_manipulation_mk_names(text=comment.content, context_status=status)
         text = processor.text_manipulation_emojis(text=text)
         comment.processed_content = text
@@ -25,13 +25,12 @@ class Command(KikarStatusCommand):
         list_of_statuses = self.parse_statuses(args, options)
         processor = TextProcessor()
         # Iterate over list_of_statuses
-        list_of_comments = Facebook_Status_Comment.objects.filter(parent__status_id__in=list_of_statuses)
-        num_of_comments = list_of_comments.count()
-        i = 0
+        i = 1
         with futures.ThreadPoolExecutor(max_workers=options['workers']) as executer:
-            for comment in list_of_comments.iterator():
-                executer.submit(self.worker, i, comment, comment.parent, processor, num_of_comments)
-                i += 1
+            for status in list_of_statuses.iterator():
+                for comment in status.comments.all():
+                    executer.submit(self.worker, i, comment, status, processor)
+                    i += 1
         info_msg = "Successfully saved all statuses to db"
         logger = logging.getLogger('django')
         logger.info(info_msg)
