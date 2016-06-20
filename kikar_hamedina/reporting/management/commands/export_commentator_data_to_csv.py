37d4f6664fde85e3874870eaa231ee01e1852385
Yotam <yotammanor@gmail.com>
2016-06-20 12:24:45 +0300

    added filter by year, calculate by feed.

--- kikar_hamedina/reporting/management/commands/export_commentator_data_to_csv.py
+++ kikar_hamedina/reporting/management/commands/export_commentator_data_to_csv.py
@@ -10,45 +10,59 @@ DELIMITER = '~'
 
 
 class Command(KikarBaseCommand):
+
+    def add_arguments(self, parser):
+        parser.add_argument('--year',
+                            action='store',
+                            dest='year',
+                            default=None,
+                            help="choose year to filter on"
+                            )
+
     def build_commentator_data(self, **kwargs):
         counter = dict()
-        for feed in Facebook_Feed.objects.all():
+        for feed in Facebook_Feed.objects.all().order_by('id'):
             counter[feed.id] = {}
             counter[feed.id]['unique'] = {'likes_2014': set(), 'likes_2015': set(), 'comments_2014': set(),
                                           'comments_2015': set()}
             counter[feed.id]['full'] = {'likes_2014': long(), 'likes_2015': long(), 'comments_2014': long(),
                                         'comments_2015': long()}
-        for status in Facebook_Status.objects.filter(**kwargs).order_by('published'):
-            if not status.is_comment:
-                year = status.published.strftime('%Y')
-                counter[status.feed.id]['unique']['likes_%s' % year] = counter[status.feed.id]['unique'][
-                    'likes_%s' % year].union(
-                    set(status.likes.values_list('user', flat=True)))
-                counter[status.feed.id]['unique']['comments_%s' % year] = counter[status.feed.id]['unique'][
-                    'comments_%s' % year].union(
-                    set(status.comments.values_list('comment_from_id', flat=True)))
-                counter[status.feed.id]['full']['likes_%s' % year] += status.likes.count()
-                counter[status.feed.id]['full']['comments_%s' % year] += status.comments.count()
-                print(status.published)
+            for status in feed.facebook_status_set.filter(**kwargs).order_by('published'):
+                if not status.is_comment:
+                    year = status.published.strftime('%Y')
+                    counter[status.feed.id]['unique']['likes_%s' % year] = counter[status.feed.id]['unique'][
+                        'likes_%s' % year].union(
+                        set(status.likes.values_list('user', flat=True)))
+                    counter[status.feed.id]['unique']['comments_%s' % year] = counter[status.feed.id]['unique'][
+                        'comments_%s' % year].union(
+                        set(status.comments.values_list('comment_from_id', flat=True)))
+                    counter[status.feed.id]['full']['likes_%s' % year] += status.likes.count()
+                    counter[status.feed.id]['full']['comments_%s' % year] += status.comments.count()
+                    print('\t', status.published)
+            print(feed.id)
         return counter
 
     def handle(self, *args, **options):
         print('Start.')
-        counter = self.build_commentator_data()
+        kwargs = None
+        if options['year']:
+            kwargs = {'published__year': options['year']}
+        counter = self.build_commentator_data(**kwargs)
         file_name = 'commentator_data_{}.csv'.format(timezone.now().strftime('%Y_%m_%d'))
         with open(file_name, 'wb') as f:
             field_names = [
+                'feed_id'
+                'link',
                 'mk_id',
                 'mk_name',
                 'mk_party',
-                'feed_id',
                 'likes_2014_unique',
-                'likes_2014_full',
                 'likes_2015_unique',
+                'likes_2014_full',
                 'likes_2015_full',
                 'comments_2014_unique',
-                'comments_2014_full',
                 'comments_2015_unique',
+                'comments_2014_full',
                 'comments_2015_full'
 
             ]
@@ -61,14 +75,15 @@ class Command(KikarBaseCommand):
                            'utf-8') if feed.persona.content_object else feed.username,
                        'mk_party': unicode(feed.persona.content_object.current_party.name).encode(
                            'utf-8') if feed.persona.content_object else None,
-                       'feed_id': feed.vendor_id,
+                       'feed_id': feed.id,
+                       'link': 'http://www.facebook.com/{}'.fornat(feed.vendor_id),
                        'likes_2014_unique': len(counter[feed.id]['unique']['likes_2014']),
-                       'likes_2014_full': counter[feed.id]['full']['likes_2014'],
                        'likes_2015_unique': len(counter[feed.id]['unique']['likes_2015']),
+                       'likes_2014_full': counter[feed.id]['full']['likes_2014'],
                        'likes_2015_full': counter[feed.id]['full']['likes_2015'],
                        'comments_2014_unique': len(counter[feed.id]['unique']['comments_2014']),
-                       'comments_2014_full': counter[feed.id]['full']['comments_2014'],
                        'comments_2015_unique': len(counter[feed.id]['unique']['comments_2015']),
+                       'comments_2014_full': counter[feed.id]['full']['comments_2014'],
                        'comments_2015_full': counter[feed.id]['full']['comments_2015']
                        }
                 csv_data.writerow(row)
