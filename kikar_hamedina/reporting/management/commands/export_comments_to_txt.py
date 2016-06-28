6704531a8b8e02a43a476c251d4eb7a9dc0b5c32
Yotam <yotammanor@gmail.com>
2016-06-28 16:15:59 +0300

    fix bugs

--- kikar_hamedina/reporting/management/commands/export_comments_to_txt.py
+++ kikar_hamedina/reporting/management/commands/export_comments_to_txt.py
@@ -1,6 +1,7 @@
 #!encoding utf-8
 from csv import DictWriter
 from facebook_feeds.management.commands.kikar_base_commands import KikarCommentCommand
+from facebook_feeds.models import Facebook_Status
 from reporting.utils import TextProcessor
 
 DELIMITER = '~'
@@ -29,13 +30,13 @@ class Command(KikarCommentCommand):
 
         i = 0
         for status in Facebook_Status.objects.all():
-            for comment in status.commentsa.all():
+            for comment in status.comments.all():
                 processed_text = comment.content
                 # processed_text = processor.text_manipulation_mk_names(text=comment.content, context_status=comment.parent)
                 if options['translate']:
                     processed_text = processor.text_manipulation_translate_text(text=processed_text)
                 processed_text = processor.text_manipulation_emojis(text=processed_text)
-                print('writing comment {} of {}'.format(i + 1, comments.count()))
+                print('writing comment {}'.format(i + 1))
                 i += 1
                 dict_row = {
                     'content': processor.text_manipulation_flatten_text(processed_text, delimiter=DELIMITER),
