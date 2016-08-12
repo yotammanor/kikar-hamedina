7db8efcd792ea14dd9ce129d6d32d415b1853335
Agam Rafaeli <agam.rafaeli@gmail.com>
2016-08-13 00:35:52 +0300

    Change mks structure to check the import problem

--- tests/test_mk.py
+++ tests/test_mk.py
@@ -1,11 +1,11 @@
 from unittest import TestCase
 
-from mks.models import Member
+import mks
 
 class MemberTestCase(TestCase):
 
     def test_create_and_delete_memeber(self):
-        new_member = Member()
+        new_member = mks.Member()
         new_member.save()
 
         new_member.delete()
