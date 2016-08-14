f8aa193da9b6320f710a0fd4bbc504858d26972e
Agam Rafaeli <agam.rafaeli@gmail.com>
2016-08-14 20:18:49 +0300

    Add test to get party page

--- tests/test_get_basic_pages.py
+++ tests/test_get_basic_pages.py
@@ -12,6 +12,7 @@ class BasicPagesTestCase(TestCase):
 
     def setUp(self):
         self.member_id = "731"
+        self.party_id = 32
 
     def test_get_homepage(self):
         response = requests.get("http://localhost:8000")
@@ -22,4 +23,8 @@ class BasicPagesTestCase(TestCase):
         response = requests.get(mk_page_url)
         self.assertEqual(response.status_code, 200)
 
+    def test_get_party_page(self):
+        party_page_url = "http://localhost:8000/party/{party_id}".format(party_id=self.party_id)
+        response = requests.get(party_page_url)
+        self.assertEqual(response.status_code, 200)
 
