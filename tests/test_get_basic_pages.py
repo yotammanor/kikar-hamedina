c883e16531f07281f938222c780d1eae577988ce
Agam Rafaeli <agam.rafaeli@gmail.com>
2016-08-14 20:20:47 +0300

    Add test to get party page, extract base_url

--- tests/test_get_basic_pages.py
+++ tests/test_get_basic_pages.py
@@ -11,20 +11,22 @@ logger = getLogger("my_logger")
 class BasicPagesTestCase(TestCase):
 
     def setUp(self):
+        self.base_url = "http://localhost:8000"
         self.member_id = "731"
-        self.party_id = 32
+        self.party_id = "32"
 
     def test_get_homepage(self):
-        response = requests.get("http://localhost:8000")
+        response = requests.get(self.base_url)
         self.assertEqual(response.status_code, 200)
 
     def test_get_mk_page(self):
-        mk_page_url = "http://localhost:8000/member/{member_id}".format(member_id=self.member_id)
+        mk_page_url = self.base_url + "/member/{member_id}".format(member_id=self.member_id)
         response = requests.get(mk_page_url)
         self.assertEqual(response.status_code, 200)
 
     def test_get_party_page(self):
-        party_page_url = "http://localhost:8000/party/{party_id}".format(party_id=self.party_id)
+
+        party_page_url = self.base_url + "/party/{party_id}".format(party_id=self.party_id)
         response = requests.get(party_page_url)
         self.assertEqual(response.status_code, 200)
 
