73627b5c19318f8373f07887c197205cd6fe8b84
Agam Rafaeli <agam.rafaeli@gmail.com>
2016-08-16 17:46:07 +0300

    Submit 3 tests and add coveralls after script

--- tests/test_get_basic_pages.py
+++ tests/test_get_basic_pages.py
@@ -13,20 +13,20 @@ class BasicPagesTestCase(TestCase):
         self.member_id = "731"
         self.party_id = "32"
 
-    # def test_get_homepage(self):
-    #     response = self.client.get("/")
-    #     self.assertEqual(response.status_code, 200)
+    def test_get_homepage(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
 
-    # def test_get_mk_page(self):
-    #     mk_page_url = "/member/{member_id}/".format(member_id=self.member_id)
-    #     response = self.client.get(mk_page_url)
-    #     self.assertEqual(response.status_code, 200)
+    def test_get_mk_page(self):
+        mk_page_url = "/member/{member_id}/".format(member_id=self.member_id)
+        response = self.client.get(mk_page_url)
+        self.assertEqual(response.status_code, 200)
 
-    # def test_get_party_page(self):
-    #     party_page_url = "/party/{party_id}/".format(party_id=self.party_id)
-    #     response = self.client.get(party_page_url)
-    #     self.assertEqual(response.status_code, 200)
-    #
+    def test_get_party_page(self):
+        party_page_url = "/party/{party_id}/".format(party_id=self.party_id)
+        response = self.client.get(party_page_url)
+        self.assertEqual(response.status_code, 200)
+
     # def test_get_search_page(self):
     #     response = self.client.get("/search/")
     #     self.assertEqual(response.status_code, 200)
