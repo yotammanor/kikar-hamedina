6273f984831a54415c8a00958c95610771c400e4
Agam Rafaeli <agam.rafaeli@gmail.com>
2016-08-16 17:08:27 +0300

    Add Facebook_feed fixtures, change to use the Django test client

--- tests/test_get_basic_pages.py
+++ tests/test_get_basic_pages.py
@@ -1,41 +1,37 @@
-from unittest import TestCase
+from django.test import TestCase
+from django.test import Client as TestingClient
 
 import django
 django.setup()
 
-import requests
-from logging import getLogger
-
-logger = getLogger("my_logger")
-
 class BasicPagesTestCase(TestCase):
 
     def setUp(self):
-        self.base_url = "http://localhost:8000"
+        self.client = TestingClient()
+
+        # Hardcoded configurations
         self.member_id = "731"
         self.party_id = "32"
 
     def test_get_homepage(self):
-        response = requests.get(self.base_url)
+        response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
 
     def test_get_mk_page(self):
-        mk_page_url = self.base_url + "/member/{member_id}".format(member_id=self.member_id)
-        response = requests.get(mk_page_url)
+        mk_page_url = "/member/{member_id}".format(member_id=self.member_id)
+        response = self.client.get(mk_page_url)
         self.assertEqual(response.status_code, 200)
 
     def test_get_party_page(self):
-        party_page_url = self.base_url + "/party/{party_id}".format(party_id=self.party_id)
-        response = requests.get(party_page_url)
+        party_page_url = "/party/{party_id}".format(party_id=self.party_id)
+        response = self.client.get(party_page_url)
         self.assertEqual(response.status_code, 200)
 
     def test_get_search_page(self):
-        search_page_url = self.base_url + "/search"
-        response = requests.get(search_page_url)
+        response = self.client.get("/search/")
         self.assertEqual(response.status_code, 200)
 
     def test_get_about_page(self):
-        about_page_url = self.base_url + "/about"
-        response = requests.get(about_page_url)
+        response = self.client.get("/about/")
         self.assertEqual(response.status_code, 200)
 
