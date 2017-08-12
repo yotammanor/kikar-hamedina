856c83c780428a84c877f2b89a25034faa42d73a
Yotam Manor <yotammanor@gmail.com>
2017-08-12 23:27:28 +0300

    KnessetFactory has get_or_create by number.

--- kikar_hamedina/mks/factories/knesset_factory.py
+++ kikar_hamedina/mks/factories/knesset_factory.py
@@ -5,5 +5,6 @@ from mks import models
 class KnessetFactory(factory.DjangoModelFactory):
     class Meta:
         model = models.Knesset
+        django_get_or_create = ('number',)
 
     number = factory.Sequence(lambda n: n)
